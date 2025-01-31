19d2a96606a49895b8358fd74f588f0af276a88a
João Luis <joaoluisd2005@gmail.com>
2025-01-31 16:05:27 -0300

    questao incompleta

--- listas_encadeadas.c
+++ listas_encadeadas.c
@@ -13,28 +13,38 @@ Lista * busca_lista(Lista * lista_atual, int valor);
 Lista * exclui_lista(Lista * lista_atual, int valor);
 void libera_lista(Lista *lista_atual);
 Lista * maximo_lista(Lista *lista_atual);
-Lista * ordena_lista(Lista *Lista_atual);
+Lista * ordena_lista_crescente(Lista *Lista_atual);
+Lista * minimo_lista(Lista *lista_atual);
+Lista * ordena_lista_decrescente(Lista *lista_atual);
 int main()
 {
     int n;
-    Lista * prim;
-    prim = cria_lista();
+    Lista * prim = cria_lista();
     prim = insere_lista(prim , 1);
     prim = insere_lista(prim , 2);
     prim = insere_lista(prim , 3);
     prim = insere_lista(prim , 4);
-    lista_vazia(prim);
     imprime_lista(prim);
+    printf("\n");
     printf("DIGITE UM ELEMENTO PARA BUSCAR NA LISTA: ");
     scanf("%d",&n);
-    
     if(busca_lista(prim , n))
     {
         printf("O ELEMENTRO %d ESTA NA LISTA\n",n);
     }
-    else printf("ESTE ELEMENTO NAO ESTA NA LISTA\n");
+    else printf("O ELEMENTO %d NAO FOI ENCONTRADO\n",n);
     
+    Lista * maioral = ordena_lista_crescente(prim);
+    
+    printf("Ordem crescente: \n");
+    imprime_lista(maioral);
+    
+    printf("\n");
+    Lista *menor = ordena_lista_decrescente(prim);
+    printf("Ordem decrescente: \n");
+    imprime_lista(menor);
 
+    
     return 0;
 }
 Lista * cria_lista()
@@ -80,18 +90,21 @@ Lista * exclui_lista(Lista * lista_atual, int valor)
 {
     Lista * lista_anterior = NULL;
     Lista * p = lista_atual;
+
     while (p != NULL && p->info != valor)
     {
         lista_anterior = p;
         p = p->prox;
     }
+
     if(p == NULL)
     {
         return lista_atual;
     }
+
     if(lista_anterior == NULL)
     {
-        p = p->prox;
+        lista_atual = p->prox;
     }
     else
     {
@@ -115,13 +128,13 @@ Lista * maximo_lista(Lista *lista_atual)
 {
     Lista * maior = lista_atual;
     
-    if(lista_atual != lista_vazia(lista_atual))
+    if(!lista_vazia(lista_atual))
     {
         Lista * p = lista_atual;
 
         while(p != NULL)
         {
-            if(maior->info > p->info)
+            if(maior->info < p->info)
             {
                 maior = p;
             }
@@ -132,17 +145,56 @@ Lista * maximo_lista(Lista *lista_atual)
     return NULL;
     
 }
-Lista * ordena_lista(Lista *lista_atual)
+Lista * ordena_lista_crescente(Lista *lista_atual)
 {
-    Lista* lista_ordenada = cria_lista(); // cria uma nova lista vazia
+    Lista* lista_crescente = cria_lista(); // cria uma nova lista vazia
 
-    while(lista_atual != lista_vazia(lista_atual))
+    while(!lista_vazia(lista_atual))
     {
         Lista * maior = maximo_lista(lista_atual);
 
-        lista_ordenada = insere_lista(lista_ordenada, maior->info);
+        lista_crescente = insere_lista(lista_crescente, maior->info);
 
         lista_atual = exclui_lista(lista_atual, maior->info);
     }
-    return lista_ordenada;
+
+    return lista_crescente;
+}
+Lista * minimo_lista(Lista *lista_atual)
+{
+    Lista * menor = lista_atual;
+    if(!lista_vazia(lista_atual))
+    {
+        Lista * p = lista_atual;
+        while(p != NULL)
+        {
+            if(menor->info > p->info)
+            {
+                menor = p;
+            }
+            p = p->prox;
+        }
+        return menor;
+    }
+    return NULL;
+}
+Lista * ordena_lista_decrescente(Lista *lista_atual)
+{
+    Lista * lista_decrescente = cria_lista();
+
+    while(!lista_vazia(lista_atual))
+    {
+        Lista * menor = minimo_lista(lista_atual);
+
+        
+        printf("\nMENOR: %d\n", menor->info);
+        
+        imprime_lista(lista_atual);
+
+
+        lista_decrescente = insere_lista(lista_decrescente, menor->info);
+        lista_atual = exclui_lista(lista_atual, menor->info);
+    }
+
+    return lista_decrescente;
 }
